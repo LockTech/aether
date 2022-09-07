d8decdb261f39f2ba6546b9a196b902b99551278
realStandal <jesselockard99@gmail.com>
2022-09-07 16:06:32 -0400

    added default prop-values for the Button component

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -1,6 +1,19 @@
 import { theme as chakraTheme } from '@chakra-ui/react'
 import type { Theme } from '@chakra-ui/react'
 
-const theme: Theme = chakraTheme
+const theme: Theme = {
+  ...chakraTheme,
+  components: {
+    ...chakraTheme.components,
+    Button: {
+      ...chakraTheme.components.Button,
+      defaultProps: {
+        colorScheme: 'teal',
+        size: 'md',
+        variant: 'solid',
+      },
+    },
+  },
+}
 
 export default theme
