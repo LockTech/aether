74116c42bb44bdf860b213ce75642b2894b66749
realStandal <jesselockard99@gmail.com>
2022-09-09 01:30:15 -0400

    added redirect to AuthLayout when users are authenticated

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react'
 import type { ReactNode } from 'react'
 
 import { Box, Stack } from '@chakra-ui/react'
 
+import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
+
 interface AuthLayoutProps {
   children?: ReactNode
+  redirect?: boolean
 }
 
-const AuthLayout = ({ children }: AuthLayoutProps) => {
+const AuthLayout = ({ children, redirect }: AuthLayoutProps) => {
+  const { isAuthenticated } = useAuth()
+
+  useEffect(
+    () => redirect && isAuthenticated && navigate(routes.dashboard()),
+    [isAuthenticated, redirect]
+  )
+
   return (
     <Box as={Stack} maxWidth="26rem" marginX="auto" paddingTop="4" spacing={8}>
       {children}
