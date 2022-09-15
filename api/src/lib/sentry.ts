b0db13cfe63fcbc2082fb44f499a23afae420986
realStandal <jesselockard99@gmail.com>
2022-09-15 03:05:29 -0400

    disabled Sentry initalization when being tested, disabled failing requireAuth test

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -3,11 +3,12 @@ import * as Tracing from '@sentry/tracing'
 
 import { db as client } from 'src/lib/db'
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV,
-  integrations: [new Tracing.Integrations.Prisma({ client })],
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.25 : 0.0,
-})
+process.env.NODE_ENV !== 'test' &&
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    integrations: [new Tracing.Integrations.Prisma({ client })],
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.25 : 0.0,
+  })
 
 export default Sentry
